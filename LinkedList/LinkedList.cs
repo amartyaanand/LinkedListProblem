2601cae2d0acd4167f89a16b76cffb567165c30e
Amartya <amartyaanand21@live.com>
2020-10-21 23:18:48 +0530

    [Amartya] Add : Create simple linked list of 56, 30 and 70.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        internal Node head;
+        public LinkedList()
+        {
+            this.head = null;
+        }
+    }
+}
