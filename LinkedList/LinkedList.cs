bd6be483ae348cb6755e1350f7ec13406f7835e4
Amartya <amartyaanand21@live.com>
2020-10-22 00:23:41 +0530

    [Amartya] Add : Search linked list to find node.

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LinkedList
 {
-    class LinkedList
+    public class LinkedList
     {
         public Node head;
         public LinkedList()
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -2,7 +2,7 @@
 
 namespace LinkedList
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
